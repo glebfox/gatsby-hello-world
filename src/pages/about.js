b1b71a3442a89e4c97c7eee96f2b8bf4de2987ca
Gleb Gorelov <gorelov@haulmont.com>
2021-04-23 17:16:46 +0400

    2. Introduction to Styling in Gatsby

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,35 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import * as styles from "./about.module.css"
+import Container from "../components/container"
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
 
 export default function About() {
   return (
-    <div style={{ color: `teal` }}>
+    <Container>
       <Link to="/">Home</Link>
       <Header headerText="About Gatsby" />
       <Header headerText="It's pretty cool" />
-      <p>Such wow. Very React.</p>
-    </div>
+      <User
+        username="Maria Randall"
+        avatar="https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/tutorial/part-two/pexels-daniel-xavier-1102341.jpg"
+        excerpt="I'm Maria Randall. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <User
+        username="Daniela Dewitt"
+        avatar="https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/tutorial/part-two/pexels-guilherme-almeida-1858175.jpg"
+        excerpt="I'm Daniela Dewitt. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+    </Container>
   )
 }
